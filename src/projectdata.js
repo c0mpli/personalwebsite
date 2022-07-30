80449e0436024691c974c441d12c793e8fada48b
Jash Doshi <shirothegamer69@gmail.com>
2022-07-30 21:23:43 +0530

    khoj added

--- src/projectdata.js
+++ src/projectdata.js
@@ -2,16 +2,26 @@ const projectList =
 [
     {
         "id":1,
-        "name":"Ally",
-        "description":"Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
-        image: require('./images/ally.jpg'),
-        "website":"",
-        "github":"https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
+        "name":"Khoj",
+        "description":"Khoj is a website to rescue pets and give them a shelter (Hackathon project).",
+        image: require('./images/khoj.png'),
+        "website":"#",
+        "github":"#",
         "tools": ["HTML", "CSS", "Javascript"],
         inView:false
     },
     {
         "id":2,
+        "name":"Ally",
+        "description":"Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
+        image: require('./images/ally.jpg'),
+        "website":"#",
+        "github":"https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
+        "tools": ["HTML", "CSS", "Javascript"],
+        inView:false
+    },
+    {
+        "id":3,
         "name":"Personal Website",
         "description":"Personal website made to showcase skills and projects",
         image: require('./images/pw.png'),
