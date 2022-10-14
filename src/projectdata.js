a12bf03e74c4a925c32fb443625188fd0deea891
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2022-10-14 18:14:56 +0530

    iete website added in project

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,27 +1,37 @@
 const projectList = 
-[
+[   
     {
         "id":1,
+        "name":"IETE-TSEC Website",
+        "description":"A website for IETE-TSEC to showcase their events, gallery, accept responses and more about them.",
+        image: require('./images/iete-website.png'),
+        "website":"https://ietetsec.in/",
+        "github":"https://ietetsec.in/",
+        "tools": ["React.js", "MongoDB", "framer-motion"],
+        inView:false
+    },
+    {
+        "id":2,
         "name":"Khoj",
         "description":"Khoj is a website to rescue pets and give them a shelter (Hackathon project).",
         image: require('./images/khoj.png'),
-        "website":"https://c0mpli.github.io/Need-for-code-23/index.html",
+        "website":"https://c0mpli.github.io/Need-for-code-23/",
         "github":"https://github.com/c0mpli/Need-for-code-23",
         "tools": ["HTML", "CSS", "Javascript"],
         inView:false
     },
     {
-        "id":2,
+        "id":3,
         "name":"Ally",
         "description":"Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
         image: require('./images/ally.jpg'),
-        "website":"#",
+        "website":"https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
         "github":"https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
         "tools": ["HTML", "CSS", "Javascript"],
         inView:false
     },
     {
-        "id":3,
+        "id":4,
         "name":"Personal Website",
         "description":"Personal website made to showcase skills and projects",
         image: require('./images/pw.png'),
