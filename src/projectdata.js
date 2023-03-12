3e4d9ee3b8d42e32738a1a104d9dd05b3a73197b
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2023-03-12 12:33:08 +0530

    Update projectdata.js

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,75 +1,79 @@
-const projectList = 
-[   {
-        "id":1,
-        "name":"Blindr",
-        "description":"A blind dating website with features like chatroom, maps, games, matching algorithms, facial verification, etc.",
-        image: require('./images/blindr.png'),
-        "website":"https://coc23.vercel.app/",
-        "github":"",
-        "tools": ["React.js","MongoDB","Node.js","Express.js"],
-        inView:false
-    },
- {
-        "id":2,
-        "name":"Mail Me",
-        "description":"Mail yourself anything without logging in.",
-        image: require('./images/mailme.png'),
-        "website":"https://mailme.c0mpli.repl.co/",
-        "github":"",
-        "tools": ["HTML","CSS","Node.js","Express.js"],
-        inView:false
-    },
- {
-        "id":3,
-        "name":"Physiotherapist Website",
-        "description":"A website for Heath Matthew Physiotherapist to help him track his user's progress on his courses.",
-        image: require('./images/HMP.png'),
-        "website":"https://project-hmp.vercel.app/",
-        "github":"",
-        "tools": ["React.js","MongoDB","Node.js","Express.js"],
-        inView:false
-    },
-    {
-        "id":4,
-        "name":"IETE-TSEC Website",
-        "description":"A website for IETE-TSEC to showcase their events, gallery, accept responses and more about them.",
-        image: require('./images/iete-website.png'),
-        "website":"https://ietetsec.in/",
-        "github":"",
-        "tools": ["React.js", "MongoDB", "Framer-motion"],
-        inView:false
-    },
-    {
-        "id":5,
-        "name":"Khoj",
-        "description":"Khoj is a website to rescue pets and give them a shelter (Hackathon project).",
-        image: require('./images/khoj.png'),
-        "website":"https://khoj-piedpiper.herokuapp.com/",
-        "github":"https://github.com/c0mpli/Need-for-code-23",
-        "tools": ["HTML", "CSS", "Javascript"],
-        inView:false
-    },
-    {
-        "id":6,
-        "name":"Ally",
-        "description":"Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
-        image: require('./images/ally.jpg'),
-        "website":"",
-        "github":"https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
-        "tools": ["HTML", "CSS", "Javascript"],
-        inView:false
-    },
-    {
-        "id":7,
-        "name":"Personal Website",
-        "description":"Personal website made to showcase skills and projects",
-        image: require('./images/pw.png'),
-        "website":"https://c0mpli.github.io/personalwebsite/",
-        "github":"https://github.com/c0mpli/personalwebsite",
-        "tools": ["React.js", "GSAP", "Framer-motion"],
-        inView:false
-    }
+const projectList = [
+  {
+    id: 1,
+    name: "Blindr",
+    description:
+      "A blind dating website with features like chatroom, maps, games, matching algorithms, facial verification, etc.",
+    image: require("./images/blindr.png"),
+    website: "https://coc23.vercel.app/",
+    github: "https://github.com/c0mpli/coc23",
+    tools: ["React.js", "MongoDB", "Node.js", "Express.js"],
+    inView: false,
+  },
+  {
+    id: 2,
+    name: "Mail Me",
+    description: "Mail yourself anything without logging in.",
+    image: require("./images/mailme.png"),
+    website: "https://mailme.c0mpli.repl.co/",
+    github: "https://github.com/c0mpli/mailMe",
+    tools: ["HTML", "CSS", "Node.js", "Express.js"],
+    inView: false,
+  },
+  {
+    id: 3,
+    name: "Physiotherapist Website",
+    description:
+      "A website for Heath Matthew Physiotherapist to help him track his user's progress on his courses.",
+    image: require("./images/HMP.png"),
+    website: "https://project-hmp.vercel.app/",
+    github: "",
+    tools: ["React.js", "MongoDB", "Node.js", "Express.js"],
+    inView: false,
+  },
+  {
+    id: 4,
+    name: "IETE-TSEC Website",
+    description:
+      "A website for IETE-TSEC to showcase their events, gallery, accept responses and more about them.",
+    image: require("./images/iete-website.png"),
+    website: "https://ietetsec.in/",
+    github: "",
+    tools: ["React.js", "MongoDB", "Framer-motion"],
+    inView: false,
+  },
+  {
+    id: 5,
+    name: "Khoj",
+    description:
+      "Khoj is a website to rescue pets and give them a shelter (Hackathon project).",
+    image: require("./images/khoj.png"),
+    website: "https://khoj-piedpiper.herokuapp.com/",
+    github: "https://github.com/c0mpli/Need-for-code-23",
+    tools: ["HTML", "CSS", "Javascript"],
+    inView: false,
+  },
+  {
+    id: 6,
+    name: "Ally",
+    description:
+      "Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
+    image: require("./images/ally.jpg"),
+    website: "",
+    github: "https://github.com/c0mpli/PiedPiper-TSEC-Hacks-2022",
+    tools: ["HTML", "CSS", "Javascript"],
+    inView: false,
+  },
+  {
+    id: 7,
+    name: "Personal Website",
+    description: "Personal website made to showcase skills and projects",
+    image: require("./images/pw.png"),
+    website: "https://c0mpli.github.io/personalwebsite/",
+    github: "https://github.com/c0mpli/personalwebsite",
+    tools: ["React.js", "GSAP", "Framer-motion"],
+    inView: false,
+  },
+];
 
-]
-
-export default projectList
+export default projectList;
