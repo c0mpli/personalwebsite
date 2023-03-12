4aa844cabd33dc5ecf3d62daeb6107eb481f8b35
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2023-03-12 12:21:53 +0530

    Update projectdata.js

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,6 +1,26 @@
 const projectList = 
 [   {
         "id":1,
+        "name":"Blindr",
+        "description":"A blind dating website with features like chatroom, maps, games, matching algorithms, facial verification, etc.",
+        image: require('./images/blindr.png'),
+        "website":"https://coc23.vercel.app/",
+        "github":"",
+        "tools": ["React.js","MongoDB","Node.js","Express.js"],
+        inView:false
+    },
+ {
+        "id":2,
+        "name":"Mail Me",
+        "description":"Mail yourself anything without logging in.",
+        image: require('./images/mailme.png'),
+        "website":"https://mailme.c0mpli.repl.co/",
+        "github":"",
+        "tools": ["HTML","CSS","Node.js","Express.js"],
+        inView:false
+    },
+ {
+        "id":3,
         "name":"Physiotherapist Website",
         "description":"A website for Heath Matthew Physiotherapist to help him track his user's progress on his courses.",
         image: require('./images/HMP.png'),
@@ -10,7 +30,7 @@ const projectList =
         inView:false
     },
     {
-        "id":2,
+        "id":4,
         "name":"IETE-TSEC Website",
         "description":"A website for IETE-TSEC to showcase their events, gallery, accept responses and more about them.",
         image: require('./images/iete-website.png'),
@@ -20,7 +40,7 @@ const projectList =
         inView:false
     },
     {
-        "id":3,
+        "id":5,
         "name":"Khoj",
         "description":"Khoj is a website to rescue pets and give them a shelter (Hackathon project).",
         image: require('./images/khoj.png'),
@@ -30,7 +50,7 @@ const projectList =
         inView:false
     },
     {
-        "id":4,
+        "id":6,
         "name":"Ally",
         "description":"Ally is a website for dementia patients to help them overcome their problem (Hackathon project).",
         image: require('./images/ally.jpg'),
@@ -40,7 +60,7 @@ const projectList =
         inView:false
     },
     {
-        "id":5,
+        "id":7,
         "name":"Personal Website",
         "description":"Personal website made to showcase skills and projects",
         image: require('./images/pw.png'),
